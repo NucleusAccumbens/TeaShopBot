9eb46e6ae8f763091bf3626b94efbc8e12f7236a
nucleus accumbens <bezlisa@mail.ru>
2022-03-02 13:00:45 +0300

    UnitOfWorkAndUserRepoChanges

---
+++ TeaShopBot/Abstractions/ITelegramBot.cs
@@ -0,0 +1,17 @@
+﻿using DATABASE.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TeaShopBot.Abstractions
+{
+    public interface ITelegramBot
+    {
+        Task<bool> SaveUserInDb(Update ubdate);
+    }
+}
---
+++ TeaShopBot/TelegramBot.cs
@@ -0,0 +1,84 @@
+﻿using DATABASE.DataContext;
+using DATABASE.Interfaces;
+using DATABASE.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TeaShopBLL;
+using TeaShopBLL.Services;
+using TeaShopBot.Abstractions;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace TeaShopBot
+{
+    public class TelegramBot : ITelegramBot
+    {
+        private static readonly string _token = "5234530253:AAE0rmehJ5dWrsrrwsdySvNjaZ25B-uibd8";
+        public ITelegramBotClient BotClient
+        {
+            get
+            {
+                return new TelegramBotClient(_token);
+            }
+        } 
+
+        private async Task<bool> CheckUserIsInDb(Update ubdate)
+        {
+            using (ShopContext context = new ShopContext())
+            {
+                try
+                {
+                    long id = ubdate.Message.Chat.Id;
+                    UnitOfWork _unit = new UnitOfWork(context);
+                    var userService = new UserService(_unit);
+
+                    return await userService.CheckUserIsInDb(id);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }              
+        }
+
+        public async Task<bool> SaveUserInDb(Update ubdate)
+        {
+            try
+            {
+                bool userIsInDb = await CheckUserIsInDb(ubdate);
+
+                if (userIsInDb == true)
+                {
+                    return false;
+                }
+                else
+                {
+                    using (ShopContext context = new ShopContext())
+                    {
+                        UnitOfWork _unit = new UnitOfWork(context);
+                        var userService = new UserService(_unit);
+                        var user = new UserDTO()
+                        {
+                            ChatId = ubdate.Message.Chat.Id,
+                            Name = ubdate.Message.Chat.Username,
+                            IsAdmin = false,
+                            IsActive = true,
+                        };                          
+                        
+                        await userService.CreateAsync(user);
+                    }
+                    return true;
+                }                  
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
